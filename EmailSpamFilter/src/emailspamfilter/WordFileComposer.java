bdebdd64deaf63168a20e07a79eff29d8763f59f
Chris Wang <chriswang1999@gmail.com>
2016-05-24 20:02:42 -0700

    file reader edits

--- EmailSpamFilter/src/emailspamfilter/WordFileComposer.java
+++ EmailSpamFilter/src/emailspamfilter/WordFileComposer.java
@@ -8,22 +8,29 @@ package emailspamfilter;
 
 /**
  *
- * @author Mitchell
+ * @author Chris
  */
+import java.util.*;
+import java.io.*;
 public class WordFileComposer {
-    public static void readFile() {
-        
+
+    public static void convertFile() {
+	String s = console.next();
+	String word = null;
+        BufferedReader br = new BufferedReader(new FileReader(s));
+	PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(s)));
+	StringTokenizer st = new StringTokenizer(br.read());
+	while((word = br.readLine()) != null){
+		word = removeCapitals(word);
+		pw.println(word);
+	}
+	pw.close();
+
     }
     
-    public static void removeCapitals() {
-        
+    public static String removeCapitals(String word) {
+        return word.toLowerCase();
     }
     
-    public static void spaceIndividualWords() {
-        
-    }
     
-    public static void writeFile() {
-        
-    }
 }
