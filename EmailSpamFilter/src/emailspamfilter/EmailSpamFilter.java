c597d22636d412a0e731c902ec6cc603669c9c8e
MW-Potential <mwu2021@gmail.com>
2016-05-24 21:18:50 -0700

    More code in EmailSpamFilter.java.

--- EmailSpamFilter/src/emailspamfilter/EmailSpamFilter.java
+++ EmailSpamFilter/src/emailspamfilter/EmailSpamFilter.java
@@ -5,6 +5,7 @@
  */
 package emailspamfilter;
 
+import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -28,6 +29,21 @@ public class EmailSpamFilter {
         System.out.println("APCS semester 2 project. This project implements a self-written version of the\n" +
                 "Naive Bayes classifier to rudimentarily determine if an email message is spam \n" +
                 "or not spam through single word probability.");
+        System.out.println();
+        System.out.println("Choose an Operation Option:");
+        System.out.println("\t1) Test a message for spam.\n"
+                + "\t2) Add a message to the training set.");
+        int operationSelector = scanner.nextInt();
+        scanner.nextLine();
+        if (operationSelector == 1) {
+            System.out.print("Please specify the filepath: ");
+
+        } else if (operationSelector == 2) {
+            System.out.print("Please specify all the URL components with spaces in between: ");
+
+        } else {
+            System.out.println("Invalid input.  Please answer with \"1\" or \"2\"");
+        }
     }
     
     public static void consoleInitial() {
